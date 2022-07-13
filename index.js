ed8b195998deecedde7a7411d574395c1a3fe496
damie824 <55003317+damie824@users.noreply.github.com>
2022-07-13 14:14:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,6 @@ const admin = adminList.split('/')
 //set Welcome Message
 const welcomeMsg = "님 환영해요!"
 
-//uptime
-function uptime(seconds){
-    function pad(s){
-        return (s < 10 ? '0' : '') + s;
-    }
-    let hours = Math.floor(seconds / (60*60));
-    let minutes = Math.floor(seconds % (60*60) / 60);
-    let second = Math.floor(seconds % 60);
-  
-    return String('Uptime is.. \n' + pad(hours) + '시간 ' + pad(minutes) + '분 ' + pad(second) + '초');
-}
-
 //log message in console when login successed
 client.on('connected', () => {
     console.log(`${client.user.username}에 로그인을 성공했습니다.`);
@@ -33,10 +21,6 @@ client.on('connected', () => {
 client.on('messageCreate', (message) => { 
     if(message.content === prefix + 'ping'){
         message.chat.sendMessage('pong!');
-    };
-
-    if (message.content === prefix + 'uptime'){
-        message.chat.sendMessage(uptime(Math.floor(process.uptime())))
     };
 });
 
